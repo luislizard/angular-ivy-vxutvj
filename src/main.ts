3df271e21d4a47694a3e38a2ff21b9bd6a0b4bf1
luislizard <55631672+luislizard@users.noreply.github.com>
2021-10-05 14:16:38 -0300

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,20 @@
-import './polyfills';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {Gatekeeper} from 'gatekeeper-client-sdk';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
-import { AppModule } from './app/app.module';
+if (environment.production) {
+    enableProdMode();
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
+Gatekeeper.configure('9966bf1b-5da5-4b55-9301-86f9f0c77aaf', {
+    googleClientID:
+        '816324818723-e2hokn0pvjgkf8jcks6quido903ukeri.apps.googleusercontent.com',
+    facebookAppID: '489915919118075'
+});
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch((err) => console.error(err));
